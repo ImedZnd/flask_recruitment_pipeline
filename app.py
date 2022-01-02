3fc733851a78ff691a86a2e36cf4c33c09a6e347
ImedzenneD <imedzenned@yahoo.fr>
2022-01-02 16:32:07 +0000

    send email update

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
-import gspread
 import smtplib
+import ssl
 from datetime import datetime, timedelta
+import gspread
+from flask import Flask
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')                        # smtp server credentials
 
 app = Flask(__name__)
 
@@ -14,19 +19,36 @@ status_list = ["Applied",
                ]
 
 
-def send_email(email_destination):
-    sender = 'elyes@datagram.ai'
+def send_email(email_destination, message_type):    # send emails get the email_destination and message_type
+    port = 465
+    smtp_server_domain_name = "smtp.gmail.com"
+    sender_mail = config.get('main', 'sender_mail')
+    password = config.get('main', 'password')
+
     email_destination = email_destination
+    if sender_mail.endswith("@datagram.ai"):
+        if message_type == "Online Test Sent":
+            message = "Thank you for applying to [Project]."
+        elif message_type == "Reminder Sent":
+            message = "You haven’t submitted your test. Everything okay?"
+        elif message_type == "Refusal Mail Sent":
+            message = "We are sorry to tell you that you did not pass the test"
+        elif message_type == "Interview Mail Sent":
+            message = "Congratulations for passing the test. You’ll have an interview with _____"
 
-    message = """From: From Person <from@fromdomain.com>
-    To: To Person <to@todomain.com>
-    Subject: SMTP e-mail test
+        ssl_context = ssl.create_default_context()
+        service = smtplib.SMTP_SSL(smtp_server_domain_name, port, context=ssl_context)
+        service.login(sender_mail, password)
 
-    This is a test e-mail message.
-    """
+        service.sendmail(sender_mail, email_destination, f"Subject: PFE Application Update\n{message}")
+
+        service.quit()
+
+    else:
+        print("bad email")
 
 
-def check_7_days_olds(old_date):
+def check_7_days_olds(old_date):                           # to verify the email sent is 7 days old
     oo = old_date.split(" ")[0].split("/")
     past = datetime(int(oo[2]), int(oo[1]), int(oo[0]))
     present = datetime.now() - timedelta(days=7)
@@ -34,7 +56,7 @@ def check_7_days_olds(old_date):
 
 
 @app.route('/')
-def hello_world():
+def pipeline_flask():
     try:
         cr = gspread.service_account(filename="credentials.json")
         sh = cr.open("Pipeline")
@@ -49,25 +71,25 @@ def hello_world():
             if item[3] not in status_list:
                 print("mess info")
                 break
-            if item[3] == "Applied":
+            elif item[3] == "Applied":
                 wks.update_cell(x, 4, 'Online Test Sent')
                 wks.update_cell(x, 5, datetime_object)
                 send_email(item[1], "Online Test Sent")
                 print(check_7_days_olds(datetime_object))
                 break
-            if item[3] == "Online Test Sent" and not check_7_days_olds(item[4]) and not item[5]:
+            elif item[3] == "Online Test Sent" and not check_7_days_olds(item[4]) and not item[5]:
                 wks.update_cell(x, 4, 'Reminder Sent')
                 wks.update_cell(x, 5, datetime_object)
                 send_email(item[1], "Reminder Sent")
                 print(check_7_days_olds(datetime_object))
                 break
-            if item[3] == "Submitted Test" and eval(item[5]) < 0.5:
+            elif item[3] == "Submitted Test" and eval(item[5]) < 0.5:
                 wks.update_cell(x, 4, 'Refusal Mail Sent')
                 wks.update_cell(x, 5, datetime_object)
                 send_email(item[1], "Refusal Mail Sent")
                 print(check_7_days_olds(datetime_object))
                 break
-            if item[3] == "Submitted Test" and eval(item[5]) > 0.5:
+            elif item[3] == "Submitted Test" and eval(item[5]) > 0.5:
                 wks.update_cell(x, 4, 'Interview Mail Sent')
                 wks.update_cell(x, 5, datetime_object)
                 send_email(item[1], "Interview Mail Sent")
@@ -79,3 +101,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
